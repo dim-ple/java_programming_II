ec7912580c2a1b887bfd1c421cacb33df5a8c788
Harrison Goehring <98855700+dim-ple@users.noreply.github.com>
2023-04-05 16:34:55 -0400

    Add files via upload

--- CircleClicker.java
+++ CircleClicker.java
@@ -5,7 +5,6 @@
 // When you click the circle, it disappears and a new random-color circle is displayed at another random location. 
 // After 20 circles are clicked, the program will display the time spent in the program
 
-import javafx.collections.ObservableList;
 import javafx.application.Application;
 import javafx.scene.Scene; 
 import javafx.scene.layout.Pane;
@@ -14,24 +13,31 @@ import javafx.stage.Stage;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
+import javafx.scene.text.Text;
+import javafx.scene.input.MouseEvent;
+
+import javafx.event.EventHandler;
+
 
 public class CircleClicker extends Application {
-
 	public void start(Stage primaryStage) {
+		
+		int clickerCounter = 0;
 		
 		Pane pane = new Pane();
 		
-		Circle circle = new Circle(randomCoordinate(), randomCoordinate(), 20);
-		circle.setFill(Color.BLUE);
+		Circle circle = new Circle(randomCoordinate(), randomCoordinate(), 10);
+		circle.setFill(Color.color(Math.random(), Math.random(), Math.random()));
 		
 		pane.getChildren().add(circle);
 		
-		
-		
 		circle.setOnMouseClicked(e -> {
 			if (circle.contains(e.getX(), e.getY())) {
+				pane.getChildren().clear();
+				circle.setCenterX(randomCoordinate());
+				circle.setCenterY(randomCoordinate());
+				circle.setFill(Color.color(Math.random(), Math.random(), Math.random()));
 				pane.getChildren().add(circle);
-				
 			}
 		});
 		
@@ -49,7 +55,9 @@ public class CircleClicker extends Application {
 	}
 	
 	private static int randomCoordinate() {
+		
 		return (int)Math.floor(Math.random() * (300 - 1 + 1));
+		
 	}
-
+	
 }

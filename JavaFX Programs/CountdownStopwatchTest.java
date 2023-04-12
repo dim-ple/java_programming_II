b4e8c2cc7ebfc93f92a178e646199f35fad29965
Harrison Goehring <98855700+dim-ple@users.noreply.github.com>
2023-04-12 13:05:58 -0400

    Add files via upload

--- JavaFX Programs/CountdownStopwatchTest.java
+++ JavaFX Programs/CountdownStopwatchTest.java
@@ -11,35 +11,65 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage; 
 
+// Imports our javafx tools to create our UI textfield used by the user to display our number and eventual countdown
 import javafx.scene.control.TextField;
 import javafx.scene.text.Font;
 
+// Imports our javafx tools to create our animation
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.util.Duration;
 
 
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+
 public class CountdownStopwatchTest extends Application {
+
 	@Override
 	public void start(Stage primaryStage) { 
 		
-		Pane pane = new Pane();
+		Pane pane = new Pane(); // Creates a new pane to paint our textfield to
 		
-		TextField tfUserSecs = new TextField("Enter the Starting Time (in seconds)");
+		TextField tfUserSecs = new TextField("0");
 		tfUserSecs.setFont(new Font("Times", 32));
 		
 		tfUserSecs.setAlignment(Pos.CENTER);
 		tfUserSecs.setPrefSize(150, 150);
 		
-		tfUserSecs.setOnAction(e -> { 
-			int totalTime = Integer.valueOf(tfUserSecs.getText());
-			totalTime *= 1000;
-			while (totalTime > 0) {
-				totalTime -= 1000;
-				tfUserSecs.setText(String.valueOf(totalTime / 1000));
+		pane.getChildren().add(tfUserSecs);
+				
+		
+		Timeline animation = new Timeline(
+				new KeyFrame(Duration.millis(1000), e -> {
+					int totalTime = Integer.valueOf(tfUserSecs.getText());
+					String currentTime = tfUserSecs.getText();
+					totalTime *= 1000;
+					if (totalTime > 0) {
+						totalTime -= 1000;
+						currentTime = String.valueOf(totalTime / 1000);
+						tfUserSecs.setText(currentTime);
+					}
+				})
+			);
+		
+	
+		tfUserSecs.setOnKeyPressed(e -> {
+			if (tfUserSecs.getText() != "0") {
+				switch (e.getCode()) {
+					case ENTER: 
+						animation.setCycleCount(Timeline.INDEFINITE);
+						animation.play();
+				} 
+			} else {
+				animation.stop();
+				
 			}
-			
 		});
 		
-		pane.getChildren().add(tfUserSecs);
+		
+		
 		
 		Scene scene = new Scene(pane, 150, 150); // Adds our pane to the application window and sets the width and height of the window.
 		primaryStage.setTitle("Countdown Stopwatch"); // Set's our stage/window title
@@ -51,4 +81,5 @@ public class CountdownStopwatchTest extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
+
 }

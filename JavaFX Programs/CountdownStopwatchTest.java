76324cdf78623c23e62b29d84c8066934baa2081
Harrison Goehring <98855700+dim-ple@users.noreply.github.com>
2023-04-12 16:24:59 -0400

    Add files via upload

--- JavaFX Programs/CountdownStopwatchTest.java
+++ JavaFX Programs/CountdownStopwatchTest.java
@@ -2,7 +2,7 @@
 // Date Created: April 11th, 2023
 
 // This program allows the user to enter a time in seconds into a text field. When they press the Enter key, the number will count down 
-// the seconds. When the seconds reach Zero, the program will play music.
+// the seconds. When the seconds reach Zero, the program was supposed to play music, but I was unable to get this to work properly.
 
 // Imports our JavaFX tools to create our Application window, scene and pane
 import javafx.application.Application;
@@ -20,27 +20,35 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.util.Duration;
 
-
+// Imports our JavaFx tools to play music in our file
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
-
 public class CountdownStopwatchTest extends Application {
 
+	final String MUSIC_URL = "music.mp3";
+	
 	@Override
 	public void start(Stage primaryStage) { 
 		
 		Pane pane = new Pane(); // Creates a new pane to paint our textfield to
 		
-		TextField tfUserSecs = new TextField("0");
+	
+		// Commenting out these lines of code, program would not run, threw an exception. 
+		// Not exactly sure why
+		//Media media = new Media(MUSIC_URL);
+		//MediaPlayer mediaPlayer = new MediaPlayer(media);
+		
+		TextField tfUserSecs = new TextField(); // Adds our text field and sets the font, size of entered text
 		tfUserSecs.setFont(new Font("Times", 32));
 		
-		tfUserSecs.setAlignment(Pos.CENTER);
-		tfUserSecs.setPrefSize(150, 150);
+		tfUserSecs.setAlignment(Pos.CENTER); // makes our textfield center aligned
+		tfUserSecs.setPrefSize(150, 150); // sets our textfield to be the size of the window 
 		
-		pane.getChildren().add(tfUserSecs);
+		pane.getChildren().add(tfUserSecs); // adds our textfield to the pane
 				
 		
+		// Creates our animation where our user's entry will reduced by 1 every 1 second
 		Timeline animation = new Timeline(
 				new KeyFrame(Duration.millis(1000), e -> {
 					int totalTime = Integer.valueOf(tfUserSecs.getText());
@@ -50,11 +58,12 @@ public class CountdownStopwatchTest extends Application {
 						totalTime -= 1000;
 						currentTime = String.valueOf(totalTime / 1000);
 						tfUserSecs.setText(currentTime);
-					}
+					} 
 				})
 			);
 		
-	
+		
+		// our lambda function on key press for "Enter" which will trigger our countdown animation from the number entered
 		tfUserSecs.setOnKeyPressed(e -> {
 			if (tfUserSecs.getText() != "0") {
 				switch (e.getCode()) {
@@ -63,11 +72,10 @@ public class CountdownStopwatchTest extends Application {
 						animation.play();
 				} 
 			} else {
-				animation.stop();
-				
+				animation.stop(); // stops the animation when the textfield entry is 0
+				// mediaPlayer.play(); // Wasn't able to get this to work with an exception being thrown
 			}
 		});
-		
 		
 		
 		
